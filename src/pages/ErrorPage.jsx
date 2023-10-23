7310a4b5047e9789a258093d30a5cc37ec639a1a
Maria Lavrinenko <marylavrinenko@gmail.com>
2023-10-23 12:47:17 +0200

    error page done

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
 function ErrorPage() {
-  return <div>ErrorPage</div>;
+  const data = [
+    {
+      picture:
+        "https://upload.wikimedia.org/wikipedia/commons/a/a5/Red_Kitten_01.jpg",
+      message: "test",
+    },
+    { picture: "", message: "" },
+    { picture: "", message: "" },
+    { picture: "", message: "" },
+    { picture: "", message: "" },
+  ];
+
+  const index = Math.floor(Math.random() * data.length);
+
+  return (
+    <>
+      <div>
+        <img src={data[index].picture} />
+      </div>
+      <div>
+        <p>{data[index].message}</p>
+      </div>
+    </>
+  );
 }
 
 export default ErrorPage;
