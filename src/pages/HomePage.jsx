53f8fd82cf294cb79ef42c37989be137a52cf462
Maria Lavrinenko <marylavrinenko@gmail.com>
2023-10-24 09:31:00 +0200

    sidebar is not working

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import "./HomePage.css";
+import Sidebar from "../components/Sidebar";
 
 function HomePage() {
   const [games, setGames] = useState([]);
@@ -24,6 +25,7 @@ function HomePage() {
 
   return (
     <>
+      {/* <Sidebar games={games} setGames={setGames} /> */}
       <ul>
         {games.map((game) => (
           <li key={game.id}>
