002ff7497b6f134ff926ca20d66c78dcd8036664
Roberta <robyfranky@hotmail.com>
2023-10-25 15:36:26 +0200

    Rectified the import

--- src/pages/LogInPage.jsx
+++ src/pages/LogInPage.jsx
@@ -1,8 +1,7 @@
-import React, { useState, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+
 import axios from "axios";
 import "./LogIn.css";
-import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 function LogInPage() {
